65c718a026c7343e316687c7f503c4feddb81990
Hanggi Anggono <anggonohanggi@gmail.com>
2025-09-02 07:11:21 +0700

    setup nativewind

--- App.tsx
+++ App.tsx
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import './global.css'
+
+const Stack = createStackNavigator()
+
+function HomeScreen() {
+  return (
+    <View className="flex-1 justify-center items-center bg-red-400">
+      <Text>Welcome to the Home Screen!</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+function DetailsScreen() {
+  return (
+    <View>
+      <Text>This is the Details Screen!</Text>
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
